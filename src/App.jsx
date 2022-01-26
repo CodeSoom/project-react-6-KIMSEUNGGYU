5d6720064ceca1e66f9fed0013ca59ff0f566328
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-26 22:40:04 +0900

    [feat] Header 렌더 퍼블리싱 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import styled from '@emotion/styled';
+
 import Header from './components/Header';
 
 export default function App() {
   return (
-    <>
+    <Container>
       <Header />
-    </>
+      <Divider />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 80%;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const Divider = styled.hr`
+  width: 200%;
+  position: absolute;
+  left: 0;
+  color: rgba(0, 0, 0, 0.15);
+  margin: 0;
+`;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,34 @@
+import styled from '@emotion/styled';
+
 export default function Header() {
   return (
-    <header>
-      <img src="assets/logo.svg" alt="logo" />
+    <HeaderWrapper>
+      <img src="/images/logo.svg" alt="logo" />
       <ul>
         <li>블로그</li>
         <li>시리즈</li>
         <li>로그인</li>
       </ul>
-    </header>
+    </HeaderWrapper>
   );
 }
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  margin: 1rem 0;
+  display: flex;
+  justify-content: space-between;
+
+  img:hover {
+    cursor: pointer;
+  }
+
+  ul {
+    display: flex;
+  }
+  li {
+    list-style: none;
+    margin-left: 1rem;
+    font-size: 1.8rem;
+  }
+`;
