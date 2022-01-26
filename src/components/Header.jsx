b23211dc0fea02d4de5a2992965fb9d0d6e65ace
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-26 21:54:15 +0900

    [feat] Header UI 기능 구현

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="assets/logo.svg" alt="logo" />
+      <ul>
+        <li>블로그</li>
+        <li>시리즈</li>
+        <li>로그인</li>
+      </ul>
+    </header>
+  );
+}
