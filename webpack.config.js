d4bdc2aa00fe050568e2fe52bf100bf7591f00d7
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2022-01-26 20:28:00 +0900

    [G-1] 개발 환경 구축하기 (#3)
    * [env] 개발 환경 구성
    - eslint, wepback 등
    - package 설정
    - json-server package 설치
    - 기본 App 컴포넌트 테스트 코드 작성

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+};
