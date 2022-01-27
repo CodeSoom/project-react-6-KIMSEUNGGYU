b2d5d1251634da25ccdfefc453000670c7275bd5
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-27 21:34:30 +0900

    [test] App 컴포넌트 임시 tag 렌더 테스트

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,11 +2,23 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
-  const { container } = render(<App />);
+describe('App', () => {
+  it('renders  Header', () => {
+    const { container } = render(<App />);
 
-  expect(container.innerHTML).toContain('<img src=');
-  expect(container).toHaveTextContent('블로그');
-  expect(container).toHaveTextContent('시리즈');
-  expect(container).toHaveTextContent('로그인');
+    expect(container.innerHTML).toContain('<img src=');
+    expect(container).toHaveTextContent('블로그');
+    expect(container).toHaveTextContent('시리즈');
+    expect(container).toHaveTextContent('로그인');
+  });
+
+  it('redners tags', () => {
+    const { queryByRole } = render(<App />);
+
+    const tags = ['태그1', '태그2', '태그3'];
+
+    tags.forEach((tag) => {
+      expect(queryByRole('button', { name: tag })).toBeInTheDocument();
+    });
+  });
 });
