4552999b687b9c655299ed52b1fcb16667f922ac
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-28 20:47:45 +0900

    [test] tagsContainer 테스트 추가 - load 될 경우 dispatch 요청 테스트 - 렌더 테스트 분기(tags 있는 경우, 없는 경우)

--- src/containers/TagsContainer.test.jsx
+++ src/containers/TagsContainer.test.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { render } from '@testing-library/react';
 
@@ -7,19 +7,54 @@ import TagsContainer from './TagsContainer';
 import TAGS from '../../fixture/tags';
 
 jest.mock('react-redux');
+jest.mock('../libs/api');
 
 describe('tagsContainer', () => {
+  const dispatch = jest.fn();
   beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
     useSelector.mockImplementation((selector) => selector({
-      tags: TAGS,
+      tags: given.tags,
     }));
   });
 
-  it('renders tags', () => {
-    const { queryByRole } = render(<TagsContainer />);
+  function renderTagsContainer() {
+    return render(
+      <TagsContainer />,
+    );
+  }
 
-    TAGS.forEach(({ name }) => {
-      expect(queryByRole('button', { name })).toBeInTheDocument();
+  context('when load', () => {
+    given('tags', () => []);
+
+    it('dispatchs loadTags', () => {
+      renderTagsContainer();
+
+      expect(dispatch).toBeCalled();
+    });
+  });
+
+  context('with tags', () => {
+    given('tags', () => TAGS);
+
+    it('renders tags', () => {
+      const { queryByRole } = renderTagsContainer();
+
+      TAGS.forEach(({ name }) => {
+        expect(queryByRole('button', { name })).toBeInTheDocument();
+      });
+    });
+  });
+
+  context('without tags', () => {
+    given('tags', () => []);
+
+    it('renders empty tag message', () => {
+      const { queryByText } = renderTagsContainer();
+
+      expect(queryByText('등록된 태그가 없어요')).toBeInTheDocument();
     });
   });
 });
