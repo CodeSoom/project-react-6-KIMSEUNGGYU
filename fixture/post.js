a466b8c4f9c99acc5d54dbc1d663a90cc33b8c48
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-30 14:51:36 +0900

    [test] PostItem 렌더 테스트 - summary 가 있는 경우 없는 경우 렌더 테스트

---
+++ fixture/post.js
@@ -0,0 +1,11 @@
+const post = {
+  id: 1,
+  title: '자료구조 - 스택',
+  summary: '자료구조 스택 요약 글',
+  contents: '자료구조 스택 블로그 글',
+  tags: ['자료구조'],
+  createdAt: '2021.04.07',
+  updateddAt: '2021.04.07',
+};
+
+export default post;
