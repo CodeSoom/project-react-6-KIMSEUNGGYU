3b385bb3f86401e2e7f920eee9461d560f678efc
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-30 15:10:05 +0900

    [test] PostsContainer 렌더 테스트 추가

--- src/containers/PostsContainer.test.jsx
+++ src/containers/PostsContainer.test.jsx
@@ -11,6 +11,8 @@ import {
   setPosts,
 } from '@modules/slice';
 
+import POSTS from '@/fixture/posts';
+
 import PostsContainer from './PostsContainer';
 
 const mockStore = configureStore(getDefaultMiddleware());
@@ -18,7 +20,7 @@ const mockStore = configureStore(getDefaultMiddleware());
 jest.mock('react-redux');
 jest.mock('../libs/api');
 
-describe('tagsContainer', () => {
+describe('PostsContainer', () => {
   let store;
 
   const dispatch = jest.fn();
@@ -27,7 +29,7 @@ describe('tagsContainer', () => {
 
     useDispatch.mockImplementation(() => dispatch);
     useSelector.mockImplementation((selector) => selector({
-      posts: [],
+      posts: given.posts,
     }));
   });
 
@@ -38,6 +40,8 @@ describe('tagsContainer', () => {
   }
 
   context('when load', () => {
+    given('posts', () => POSTS);
+
     beforeEach(() => {
       store = mockStore({});
     });
@@ -54,6 +58,33 @@ describe('tagsContainer', () => {
       expect(actions).toEqual([
         setPosts([]),
       ]);
+    });
+  });
+
+  context('with posts', () => {
+    given('posts', () => POSTS);
+
+    it('renders posts', () => {
+      const { container } = renderPostsContainer();
+
+      POSTS.forEach(({
+        title, contents, tags, createdAt,
+      }) => {
+        expect(container).toHaveTextContent(title);
+        expect(container).toHaveTextContent(contents);
+        expect(container).toHaveTextContent([tags]);
+        expect(container).toHaveTextContent(createdAt);
+      });
+    });
+  });
+
+  context('without posts', () => {
+    given('posts', () => []);
+
+    it('renders empty posts message', () => {
+      const { container } = renderPostsContainer();
+
+      expect(container).toHaveTextContent('등록된 post 가 존재하지 않습니다');
     });
   });
 });
