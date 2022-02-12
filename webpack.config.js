639afc2cb21b75978e2343d559423a0bc0a05793
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-02-12 09:13:57 +0900

    [fix] 배포 사이트 에러 수정 - webpack public path 수정

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: process.env.REACT_APP_MODE === 'production' ? '' : '/',
   },
   module: {
     rules: [
